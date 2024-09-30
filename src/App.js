bf6a7db34390a3c0e53fc72bf78c7940377d84ee
JaquelineCarmona0597 <jaquelinecarmona546@gmail.com>
2024-09-29 19:44:52 -0600

    ignorar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+// PrimeReact CSS
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; 
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import Registro from './componentes/Registro';
 import Login from './componentes/Login';
+import DashboardLayoutBasic from './componentes/dashboardCoordi';
 
 function App() {
   const location = useLocation();
@@ -13,6 +18,8 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Registro />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<DashboardLayoutBasic />} />
+
       </Routes>
     </AnimatePresence>
   );
